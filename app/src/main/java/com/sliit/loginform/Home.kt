a81c5d038146ad248639e2673066c2ef3e1833b1
Rashika1998 <it18213140@my.sliit.lk>
2020-05-24 21:11:39 +0530

    Kotlin lab sheet has been created.

---
+++ app/src/main/java/com/sliit/loginform/Home.kt
@@ -0,0 +1,13 @@
+package com.sliit.loginform
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class Home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+}
